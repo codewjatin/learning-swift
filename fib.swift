f987eb11057fc49afad87d4ba77fd8acd0e26a8b
Jatin Kumar <main.jatink@gmail.com>
2022-10-04 21:45:08 +0530

    😉

--- fib.swift
+++ fib.swift
@@ -3,11 +3,13 @@ let num = Int(readLine()!)
 
 var fib = [0, 1]
 
-if var num {
-    while num > 0 {
+if var num, num > 2 {
+    while num > 2 {
         let sum = fib[fib.endIndex - 2] + fib[fib.endIndex - 1]
         fib.append(sum)
         num -= 1
     }
     print(fib)
+} else {
+    print("There's a else case here!")
 }
